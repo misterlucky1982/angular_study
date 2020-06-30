5ff23c9d270ea8496f6bde4c1724bae3319f635a
misterlucky1982 <misterlucky@tut.by>
2020-06-30 16:16:56 +0300

    task preporation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  clicks: number = 0;
-  array: Array<string> = ['first', 'second', 'third', 'fourth', 'fifth'];
-  onChanged(increased:any){
-    console.log("I am here");    
-    increased==true?(this.clicks==4?this.clicks=0:this.clicks++):(this.clicks==0?this.clicks=4:this.clicks--);
-        console.log(this.clicks);
-    }
-}
